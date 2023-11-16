24d613a9991a614d26c766c0e69f99f49689ad68
Oleg <Oleg-BLR@yandex.ru>
2023-11-16 08:20:49 +0300

    Task 2 solved comments 02

--- src/com/teachmeskills/lesson05/Task2.java
+++ src/com/teachmeskills/lesson05/Task2.java
@@ -1,7 +1,5 @@
 package com.teachmeskills.lesson05;
 
-import java.util.Arrays;
-
 public class Task2 {
     public static void main(String[] args) {
 
@@ -25,20 +23,6 @@ public class Task2 {
         }
         System.out.println();
 
-//        //multiply matrix1 and matrix2 and output result in console
-//        int[][] result = new int[matrix1.length][matrix2[0].length];
-//        for (int i = 0; i < matrix1.length; i++) {
-//            for (int j = 0; j < matrix1[0].length; j++) {
-//                for (int k = 0; k < matrix2[0].length; k++) {
-//                    result[i][k] += matrix1[i][j] * matrix2[j][k];
-//                }
-//            }
-//        }
-//        for (int i = 0; i < result.length; i++) {
-//            System.out.println(Arrays.toString(result[i]));
-//        }
-//        System.out.println();
-
         //multiply matrix1 and matrix2
         int[][] result2 = new int[matrix1.length][matrix2[0].length];
         for (int i = 0; i < matrix1.length; i++) {
@@ -48,6 +32,7 @@ public class Task2 {
                 }
             }
         }
+        //output result2 in console in normal view
         for (int i = 0; i < result2.length; i++) {
             for (int j = 0; j < result2[i].length; j++) {
                 System.out.print(result2[i][j] + " ");
